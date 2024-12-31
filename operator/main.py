eb062a4c37a22639acb23800d2a8f569d79612b2
Jose Macchi <jose.macchi@gmail.com>
2024-12-31 17:20:50 -0300

    Added ssh key capabilities

--- operator/main.py
+++ operator/main.py
@@ -19,31 +19,43 @@ ROLLOUT_DEPLOYMENT = os.getenv('ROLLOUT_DEPLOYMENT', 'nginx')
 ROLLOUT_NAMESPACE = os.getenv('ROLLOUT_NAMESPACE', 'default')
 GIT_USERNAME = os.getenv('GIT_USERNAME', '')
 GIT_TOKEN = os.getenv('GIT_TOKEN', '')
+GIT_SSH_COMMAND = os.getenv('GIT_SSH_COMMAND', '')
 
 def get_latest_commit():
     git_url = GIT_REPO_URL
-    
-    # Modify URL to include credentials if both username and token are provided
-    if GIT_USERNAME and GIT_TOKEN:
-        # Parse the URL to insert credentials
+
+    if GIT_SSH_COMMAND:
+        logging.info("Using SSH for git access")
+    elif GIT_USERNAME and GIT_TOKEN:
         protocol_separator = "://"
         protocol, repo_path = git_url.split(protocol_separator)
         git_url = f"{protocol}://{GIT_USERNAME}:{GIT_TOKEN}@{repo_path}"
         logging.info("Using authenticated git access")
     else:
         logging.info("Using anonymous git access")
-    
+
     result = subprocess.run(
         ["git", "ls-remote", git_url, f"refs/heads/{GIT_BRANCH}"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
-    )    
+        env={**os.environ, 'GIT_SSH_COMMAND': GIT_SSH_COMMAND} if GIT_SSH_COMMAND else None
+    )
+
     if result.returncode != 0:
         logging.error(f"Git command failed: {result.stderr}")
         raise Exception("Failed to get latest commit")
         
     return result.stdout.split()[0]
+
+def show_ssh_key():
+    ssh_key_path = '/root/.ssh/id_rsa'
+    try:
+        with open(ssh_key_path, 'r') as file:
+            ssh_key_content = file.read()
+        logging.info(f"SSH key content:\n{ssh_key_content}")
+    except Exception as e:
+        logging.error(f"Error reading SSH key: {e}")
 
 def trigger_rollout(deployment_name, namespace):
     logging.info(f"Triggering rollout for {deployment_name} in {namespace}...")
@@ -57,6 +69,13 @@ def main():
     logging.info(f"Poll interval: {POLL_INTERVAL} seconds")
     logging.info(f"Rollout deployment: {ROLLOUT_DEPLOYMENT}")
     logging.info(f"Rollout namespace: {ROLLOUT_NAMESPACE}")
+
+    if GIT_SSH_COMMAND:
+        logging.info("SSH command is set, using SSH keys for authentication.")
+    else:
+        logging.info("SSH command is not set, not using SSH keys.")
+
+    show_ssh_key()
     
     last_commit = get_latest_commit()
     logging.info(f"Initial commit: {last_commit}")
